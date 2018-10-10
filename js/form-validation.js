99fba011af8574f1b0ef504019a67f841073703d
Gabriel Vilela Serra <gvs.webdev@gmail.com>
2018-10-10 11:38:18 -0300

    Added Masking Plugin

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,3 +1,16 @@
+var SPMaskBehavior = function (val) {
+  return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+},
+
+spOptions = {
+  onKeyPress: function(val, e, field, options) {
+      field.mask(SPMaskBehavior.apply({}, arguments), options);
+    }
+};
+$(document).ready(function(){
+  $("[name='telefone']").mask(SPMaskBehavior, spOptions);
+  });
+
 $("form").submit(function(event) {
     $activeForm = $("#proposal-block").find('.form-active').find('form').serializeArray();
     $name = $activeForm[0]["value"];
