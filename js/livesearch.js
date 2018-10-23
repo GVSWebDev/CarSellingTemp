e515020ce81ba24591202d52720b5db57099f6cf
Gabriel Vilela Serra <gvs.webdev@gmail.com>
2018-10-23 11:33:33 -0300

    Added Concat to database to merge both marca and nome + updated code

--- js/livesearch.js
+++ js/livesearch.js
@@ -1,9 +1,12 @@
 function getStr($str){
-    if ($str.length > 3){
+    if ($str.length >= 3){
         console.log("Buscando por: "+$str);
         $.ajax({url: "livesearch.php?q="+$str,
         success: function(result){
             console.log("AJAX foi completo! Retornado: "+ result);
+            if ($("#searchbox-results").length){
+                $("#searchbox-results").remove();
+            }
             $("#searchbox-c").append(result);
         }
     })
--- livesearch.php
+++ livesearch.php
@@ -1,14 +1,14 @@
 <?php
 require "dbconnect.php";
 
-$sql = "SELECT * FROM gvswebde_pdb.carros WHERE nome LIKE '%".$_GET['q']."%'";
+$sql = "SELECT * FROM gvswebde_pdb.carros WHERE nomefull LIKE '%".$_GET['q']."%'";
 
 $result = $con->query($sql);
 if ($result->num_rows === 0) exit ("");
 echo "<div id='searchbox-results'>";
 while($row = $result->fetch_assoc()){
     echo "<div class='searchbox-resitem'>
-    <p>".$row['nome']."</p>
+    <p>".$row['nomefull']."</p>
 </div>";
 }
 echo "</div>";
