e22d60d01d810b715f50dda5270b114ade983f52
Gabriel Vilela Serra <gvs.webdev@gmail.com>
2018-10-28 09:52:39 -0300

    Added timer to stop auto scroll action once user clicks arrow

--- js/destaque-slide.js
+++ js/destaque-slide.js
@@ -1,4 +1,5 @@
 var intervalID3 = setInterval(scrollDecide, 11500);
+var intervalID4;
 var direction = 0;
 var lastPos = 0;
 
@@ -10,19 +11,34 @@ function scrollDecide(){
         direction = 0
     }
     if (direction == 0){
-        scrollToRight();
+        scrollToRight(false);
     } else {
-        scrollToLeft();
+        scrollToLeft(false);
     }
 }
 
-function scrollToRight(){
+function bringAutoScrollBack(){
+    intervalID3 = setInterval(scrollDecide, 11500);
+    clearInterval(intervalID4);
+}
+
+function scrollToRight(byUserClick){
+    if (byUserClick == true){
+        clearInterval(intervalID3);
+        clearInterval(intervalID4);
+        intervalID4 = setInterval(bringAutoScrollBack, 10000);
+    }
     var curPos = $('#destaque-container').scrollLeft();
         $('#destaque-container').animate({scrollLeft: curPos + 2 + $(".rec-item").outerWidth(true)}, 500);
         lastPos = $('#destaque-container').scrollLeft();
 }
 
-function scrollToLeft(){
+function scrollToLeft(byUserClick){
+    if (byUserClick == true){
+        clearInterval(intervalID3);
+        clearInterval(intervalID4);
+        intervalID4 = setInterval(bringAutoScrollBack, 10000);
+    }
     var curPos = $('#destaque-container').scrollLeft();
         $('#destaque-container').animate({scrollLeft: curPos - 2 - $(".rec-item").outerWidth(true)}, 500);
         lastPos = $('#destaque-container').scrollLeft();
